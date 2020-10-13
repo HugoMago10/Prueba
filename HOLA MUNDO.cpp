503f679789e39a0db0fba22139edda60adbb2a31
AntonioQWERTY <72779711+AntonioQWERTY@users.noreply.github.com>
2020-10-12 23:39:31 -0600

    Add files via upload

---
+++ HOLA MUNDO.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+ int main()//Funcion de tipo entero, por lo tanto tiene que retornar algo para que el programa que ha finalizado correctamente, para eso es el RETURN
+ {
+ 	cout<<"HOLA MUNDO :D"<<endl;
+ 	//cout<<"HOLA MUNDO :D\n"; FOrma diferente de hacer un enter
+	return 0;
+ }
